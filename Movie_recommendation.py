8aec46477398b662bed675ac2ce5b2becb8b96c7
Tanishq Kumar Tripathi <115180949+TanishqTripathi@users.noreply.github.com>
2025-07-23 11:47:43 +0530

    Update Movie_recommendation.py

--- Movie_recommendation.py
+++ Movie_recommendation.py
@@ -1,37 +1,35 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import requests
 
-# def fetch_poster(movie_id):
-#     response = requests.get()
-    
+# Recommend top 5 similar movies
 def recommend(movie):
-    movie_idx = movies[movies['title']==movie].index[0]
+    movie_idx = movies[movies['title'] == movie].index[0]
     distance = similarity[movie_idx]
-    movie_list = sorted(list(enumerate(distance)),reverse=True,key=lambda x:x[1])[1:6]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
     
     recommended_movies = []
-    recommended_movies_poster = []
-    
     for i in movie_list:
-        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_movies_poster.append(fetch_poster(movie_id))
+    
     return recommended_movies
 
-st.title("Movie Recommendation System")
+# UI Title
+st.title("🎬 Movie Recommendation System")
+
+# Load data
 movie_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movie_dict)
-
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
+# Movie dropdown
 selected_movie = st.selectbox(
     'Select a movie to get recommendations:',
     movies['title'].values
 )
 
+# Show recommendations
 if st.button('Get Recommendations'):
     recommendations = recommend(selected_movie)
-    for i in recommendations:
-        st.write(i)
+    for name in recommendations:
+        st.write("👉", name)
